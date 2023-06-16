20b604d166c5840e4aad13a8383e39a38a79335e
yuzhen <caiyuzhen00@gmail.com>
2023-06-16 13:39:09 +0800

    base

--- Node-basic/Express框架/NoteBookExercise/AccountNote/routes/index.js
+++ Node-basic/Express框架/NoteBookExercise/AccountNote/routes/index.js
@@ -4,14 +4,15 @@ var router = express.Router()
 // 记帐本列表
 router.get('/account', function(req, res, next) {
 	// res.send('记帐本列表')
-	res.render('list')
+	res.render('list.ejs')
 })
 
 
 
 // 添加记录
 router.get('/account/create', function(req, res, next) {
-	res.send('添加记录')
+	// res.send('添加记录')
+	res.render('create.ejs')
 })
 
   
